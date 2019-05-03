da5ea509bc262ccfdcf5cdf52018f7fa109ffc00
przemyslawkulak <przemyslaw.kulak86@gmail.com>
2019-05-03 10:44:22 +0200

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { OsobyService } from './osoby.service'
+import { OsobyService } from './osoby.service';
+import { AppRoutingModule } from './app-routing.module';
+import { OneComponent } from './one/one.component';
+import { TwoComponent } from './two/two.component'
 
 @NgModule({
   declarations: [
     AppComponent,
+    OneComponent,
+    TwoComponent,
     
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    AppRoutingModule,
   ],
   providers: [OsobyService],
   bootstrap: [AppComponent]
